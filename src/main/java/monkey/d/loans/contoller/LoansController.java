4f147c11cd26fcc8d05ad48dafb64fb5ba2e757f
Harshit <harshit.3851@gmail.com>
2025-01-14 12:09:33 +0530

    added prometheus for metrics and heath

--- src/main/java/monkey/d/loans/contoller/LoansController.java
+++ src/main/java/monkey/d/loans/contoller/LoansController.java
@@ -2,6 +2,7 @@ package monkey.d.loans.contoller;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,11 @@ public class LoansController {
     public ResponseEntity<ResponseDto> applyForLoan(@RequestParam String mobileNumber, @RequestParam int amount) {
         loansService.applyLoan(mobileNumber, amount);
         return ResponseEntity.ok(new ResponseDto("Loan application successful", "success"));
+    }
+
+    @GetMapping(value = "/helloworld")
+    public ResponseEntity<ResponseDto> helloWorld() {
+        return ResponseEntity.ok(new ResponseDto("Hello World from loan ms", "success"));
     }
     
 }
